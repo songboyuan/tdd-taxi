26d1dde378a00671fb163d4b3b510d1431f7decb
Tim Song <Tim.Song@activenetwork.com>
2020-02-16 09:58:20 +0800

    Refact the app runner

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -9,35 +9,45 @@ public class AppRunner {
 
     public static void main(String[] args) {
         String testDataFile = args[0];
+        String receipt = getReceipt(testDataFile);
+        System.out.println(receipt);
+    }
+
+    private static String getReceipt(String  fileName) {
+        String receipt = "";
+        String input = readInput(fileName);
+        String[] tests = input.split("\n");
+        DistanceAndWaitParser parser = new DistanceAndWaitParser();
+        TaxiPriceCalculator calculator = new TaxiPriceCalculator();
+
+        for(String test:tests) {
+            int distance = parser.getDistance(test);
+            int wait = parser.getWaitTime(test);
+            double price = calculator.getPrice(distance, wait);
+            receipt += "收费" + Math.round(price) + "\n";
+        }
+
+        return receipt;
+
+    }
+
+
+    private static String readInput(String testDataFile) {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        byte[] buffer = new byte[1024];
+        ByteArrayOutputStream testCases = new ByteArrayOutputStream();
+        String testsDescription = "";
         try {
-            String input = readInput(testDataFile);
-            String[] tests = input.split("\n");
-            DistanceAndWaitParser parser = new DistanceAndWaitParser();
-            TaxiPriceCalculator calculator = new TaxiPriceCalculator();
-            String receipt = "";
-            for(String test:tests) {
-                int distance = parser.getDistance(test);
-                int wait = parser.getWaitTime(test);
-                double price = calculator.getPrice(distance, wait);
-                receipt += "收费" + Math.round(price) + "\n";
+            InputStream in = classLoader.getResourceAsStream(testDataFile);
+            int length;
+            while ((length = in.read(buffer)) != -1) {
+                testCases.write(buffer, 0, length);
             }
-            System.out.println(receipt);
+            testsDescription = testCases.toString("UTF-8");
         } catch (IOException e) {
             System.out.println(e.getLocalizedMessage());
         }
-    }
-
-    public static String readInput(String testDataFile) throws IOException {
-
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        InputStream in = classLoader.getResourceAsStream(testDataFile);
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = in.read(buffer)) != -1) {
-            result.write(buffer, 0, length);
-        }
-        return result.toString("UTF-8");
+        return testsDescription;
     }
 
 }
